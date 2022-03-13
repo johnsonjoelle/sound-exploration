33cb0320eaf71f35206f1bfb1bac87d5f3125e4d
Joelle Johnson <78379264+johnsonjoelle@users.noreply.github.com>
2022-03-13 18:53:28 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,14 @@
-bluetooth.onBluetoothConnected(function () {
-    basic.showIcon(IconNames.EigthNote)
-    connected = true
-    basic.clearScreen()
-    basic.showString("Talk")
-    while (connected == true) {
-        led.plotBarGraph(
-        input.soundLevel(),
-        255
-        )
-    }
-})
-bluetooth.onBluetoothDisconnected(function () {
-    connected = false
-})
 input.onSound(DetectedSound.Loud, function () {
     bluetooth.uartWriteNumber(1)
 })
 input.onSound(DetectedSound.Quiet, function () {
     bluetooth.uartWriteNumber(0)
 })
-let connected = false
 basic.showIcon(IconNames.Happy)
 bluetooth.startUartService()
+basic.forever(function () {
+    led.plotBarGraph(
+    input.soundLevel(),
+    255
+    )
+})
